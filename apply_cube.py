eff270ee343546f0ab152c19c33c139aef9023e3
alliecaton <alliecaton3@gmail.com>
2023-11-10 19:08:38 -0800

    Tested out more luts

--- apply_cube.py
+++ apply_cube.py
@@ -1,26 +1,18 @@
 from PIL import Image
 from pillow_lut import load_cube_file
-import cv2
-import numpy as np
 
 def apply(filename):
   original_file_path = 'assets/' + filename
   img = Image.open(original_file_path)
 
-  lut = load_cube_file('assets/Presetpro - Emulation.cube')
-  img.filter(lut).save(original_file_path)
+  # lut = load_cube_file('assets/Lutify.me-Claire-ACEScct-to-ACEScct.cube')
+  # lut = load_cube_file('assets/Lutify.me-Claire-RWG-to-RWG.cube')
+  # lut = load_cube_file('assets/Lutify.me-FilmTone3-ACEScct-to-ACEScct.cube')
+  # lut = load_cube_file('assets/Lutify.me-PrintFilm2-RWG-to-RWG.cube')
+  cubefile = 'Lutify.me-Claire-ACEScct-to-ACEScct'
+  lut = load_cube_file('assets/'+cubefile+'.cube')
+  
+  filename = cubefile+'.jpg'
+  img.filter(lut).save(filename)
 
-  # Add noise to image
-  image = cv2.imread(original_file_path)
-
-  mean = 0
-  stddev = 180
-  noise = np.zeros(image.shape, np.uint8)
-  cv2.randn(noise, mean, stddev)
-
-  noisy_img = cv2.add(image, noise)
-
-  # Save noisy image
-  cv2.imwrite(original_file_path, noisy_img)
-
-apply('ca.png')
+apply('test.jpg')
